85eced0e9994f13b414b4535226f753eafadafa6
SamarthBonthala <bsamarth2@gmail.com>
2019-03-07 00:07:55 +0530

    Completed area computation through Stockmeyer algo

--- 15EC143_225_Floorplanning/stockmeyer.py
+++ 15EC143_225_Floorplanning/stockmeyer.py
@@ -4,10 +4,82 @@
 # Output: List of blocks with the optimal dimensions and the co-ordinate of the lower left corner of each block
 
 
+def Stockmeyer(polish_exp, block_size):
+	# Construct a tree from the polish expression
+	polish_exp_temp = [2,5,'V',1,'H',3,7,4,'V','H',6,'V',8,'V','H']
+	#polish_exp_temp = [1,2,'V',3,'V',4,'V',5,'V',6,'V',7,'V',8,'V']
+	print polish_exp_temp
+	operands = []
+	operators = ['H','V']
+	block_sizes = [[2,4],[1,3],[3,3],[3,5],[3,2],[5,3],[1,2],[2,4]]
+	for i in polish_exp_temp:
+		if i in operators:
+			continue
+		else:
+			operands.append(i)
+		
+	no_of_blocks = len(operands)
+	co_ord = dict()
+	
+	# Parsing through the Polish expression is same as Post order traversal of a binary tree
+	# Stack creation to find the minimum area of the block
+	# Convention: xyH means x is below y and xyV means x is to the left of y
+	
+	stack = []
+	stk_blk = []
+	i = 0
+	j = 0
+	# Computing the area occupied by the orientation of all blocks for the above Polish expression
+	while(i < len(polish_exp_temp)):
+		if (polish_exp_temp[i] in operands):
+			stack.append(block_sizes[(polish_exp_temp[i]-1)])
+			print stack # Appending block sizes to the array
+		if (polish_exp_temp[i] in operators and polish_exp_temp[i] == 'H'):
+			right = stack.pop()
+			print "Right",right
+			left = stack.pop()
+			print "Left",left
+			arr = horizontal_simple(left,right)
+			stack.append(arr)
+			print stack
+		if (polish_exp_temp[i] in operators and polish_exp_temp[i] == 'V'):
+			top = stack.pop()
+			bottom = stack.pop()
+			arr = vertical_simple(top,bottom)
+			stack.append(arr)
+		i = i+1
+	size = stack.pop()
+	area = size[0]*size[1] # Area spanned by this configuration
+	
+	return area,
 
+def vertical_simple(L,R):
+	L_new = L[:]
+	R_new = R[:]
+	dim = []
+	dim.append(L_new[0]+R_new[0]) # Add the widths as the two blocks are seperated by vertical cut
+	dim.append(max(L_new[1],R_new[1])) # Consider the maximum height possible 
+	
+	return dim
 
+def horizontal_simple(L,R):
+	L_new = L[:]
+	R_new = R[:]
+	dim = []
+	dim.append(max(L_new[0],R_new[0])) # Consider the maximum width possible
+	dim.append(L_new[1]+R_new[1]) # Add the heights as the two blocks are seperated by vertical cut
+	
+	return dim
+	
+def main():
 
-# Parsing through the Polish expression is same as Post order traversal of a binary tree
+	L = [2,3]
+	R = [2,4]
+	arr =[]
+	Stockmeyer(2,3)
+	
+
+main()
 
 def vertical(L,R): # L and R are lists containing the possible dimensions of the two blocks under consideration 
 	# Sort the double dimensional array in increasing order of widths
@@ -43,14 +115,3 @@ def vertical(L,R): # L and R are lists containing the possible dimensions of the
 				#j = j+1
 				i = i+1
 			comb_no.append(temp_arr1)
-
-
-
-	print (combinations)
-	print (comb_no)
-
-
-L = [[2,3],[3,2]]
-R = [[2,4],[4,2]]
-
-vertical(L,R)
